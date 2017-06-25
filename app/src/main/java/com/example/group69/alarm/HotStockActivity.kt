a94f31577076d57095226e584b73c677c09bddc5
Nick <nick1191994@yahoo.com>
2017-06-24 23:06:32 -0400

    hotstocks activity

---
+++ app/src/main/java/com/example/group69/alarm/HotStockActivity.kt
@@ -0,0 +1,17 @@
+package com.example.group69.alarm
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by nick1_000 on 6/24/2017.
+ */
+
+class HotStockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hotstocks)
+
+    }
+}
