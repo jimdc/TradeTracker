6ca456feae16265edafd338fa143f7d655a83f34
James Carroll <jamescarroll@protonmail.ch>
2017-07-02 16:39:09 -0400

    further Kotlinize

---
+++ app/src/main/java/com/example/group69/alarm/AlertReceiver.kt
@@ -0,0 +1,72 @@
+package com.example.group69.alarm
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.app.NotificationCompat
+
+class AlertReceiver : BroadcastReceiver() {
+
+    // Called when a broadcast is made targeting this class
+    override fun onReceive(context: Context, intent: Intent) {
+
+        createNotification(context,
+                context.resources.getString(R.string.timesup),
+                context.resources.getString(R.string.fivepass),
+                context.resources.getString(R.string.alert)
+        )
+
+    }
+
+    /* public void GenerateNotify(Context context) {
+
+        NotificationManager myNotificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new Notification(android.R.drawable.ic_btn_speak_now,"hi",100);
+        Intent intent=new Intent(context.getApplicationContext(),as.class);
+        PendingIntent contentintent=PendingIntent.getBroadcast(context.getApplicationContext(),0, intent, 0);
+        notification.setLatestEventInfo(getApplicationContext(), "Hi","date", contentintent);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notification.sound = Uri.parse("android.resource://com.example.serviceproject/" + R.raw.kalimba);
+        myNotificationManager.notify(NOTIFICATION_ID,notification);
+    } */
+    fun createNotification(context: Context, msg: String, msgText: String, msgAlert: String) {
+
+        // Define an Intent and an action to perform with it by another application
+        val notificIntent = PendingIntent.getActivity(context, 0,
+                Intent(context, MainActivity::class.java), 0)
+
+        // Builds a notification
+        val mBuilder = NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ntt_logo_24_24)
+                .setContentTitle(msg)
+                .setTicker(msgAlert)
+                .setContentText(msgText)
+
+        // Defines the Intent to fire when the notification is clicked
+        mBuilder.setContentIntent(notificIntent)
+
+        // Set the default notification option
+        // DEFAULT_SOUND : Make sound
+        // DEFAULT_VIBRATE : Vibrate
+        // DEFAULT_LIGHTS : Use the default light notification
+        //mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        val sound = Uri.parse("android.resource://" + "com.example.group69.alarm" + "/" + R.raw.new_loud_ringtone)
+        mBuilder.setSound(sound)
+        mBuilder.setDefaults(Notification.DEFAULT_VIBRATE)
+        mBuilder.setDefaults(Notification.DEFAULT_LIGHTS)
+        // Auto cancels the notification when clicked on in the task bar
+        mBuilder.setAutoCancel(true)
+        //  Notification note = mBuilder.build();
+
+        // Gets a NotificationManager which is used to notify the user of the background event
+        val mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Post the notification
+        mNotificationManager.notify(1, mBuilder.build())
+
+    }
+}
---
+++ app/src/main/java/com/example/group69/alarm/MoreInfoNotification.kt
@@ -0,0 +1,14 @@
+package com.example.group69.alarm
+
+import android.app.Activity
+import android.os.Bundle
+
+
+// Called when the notification is clicked on in the task bar
+class MoreInfoNotification : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.more_info_notific)
+    }
+}
