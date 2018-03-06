c84d04c92b3c87ea4be7b50eaaffc20d40fe3eb8
James Carroll <jamescarroll@protonmail.ch>
2018-03-05 21:32:34 -0500

    play with TestSubscriber in StockDaotest

--- app/src/androidTest/kotlin/StockDaotest.kt
+++ app/src/androidTest/kotlin/StockDaotest.kt
@@ -1,17 +1,18 @@
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.arch.lifecycle.LiveData
 import com.example.group69.alarm.Stock
 import com.example.group69.alarm.StockDao
 import com.example.group69.alarm.StockDatabase
+import io.reactivex.Flowable
+import io.reactivex.Observer
+import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-/**
- * For some reason, not running...
- */
 
 @RunWith(AndroidJUnit4::class)
 open class StockDaoTest {
@@ -40,9 +41,13 @@ open class StockDaoTest {
         var ssget = stockDao.findStockById(samplestock1.stockid) //Ensure that it's not just cached.
         assert(ssget == null)
 
+        var subscriber: TestSubscriber<List<Stock>> = stockDao.getAllStocksF().test()
+
         stockDao.insert(samplestock1)
         ssget = stockDao.findStockById(samplestock1.stockid)
         assert(ssget == samplestock1)
+
+        assert(!subscriber.hasSubscription()) //For now...
     }
 
 
